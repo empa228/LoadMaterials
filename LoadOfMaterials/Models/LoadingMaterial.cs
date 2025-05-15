7e8f8535d7021672598f419811b26ce181c59bd3
Александр Ольшанский <alex.olshanskii2006@gmail.com>
2025-05-16 01:11:05 +0500

    Подключили БД

---
+++ LoadOfMaterials/Models/LoadingMaterial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoadOfMaterials.Models;
+
+public partial class LoadingMaterial
+{
+    public int Id { get; set; }
+
+    public DateTime TimeNum { get; set; }
+
+    public string TimeStr { get; set; } = null!;
+
+    public int BatchNr { get; set; }
+
+    public string StoneRec { get; set; } = null!;
+
+    public double Stone { get; set; }
+
+    public double Dolomite { get; set; }
+
+    public double Briquette { get; set; }
+
+    public double Reserve { get; set; }
+
+    public string CokeRec { get; set; } = null!;
+
+    public double Coke { get; set; }
+
+    public double StoneSp { get; set; }
+
+    public double DolomiteSp { get; set; }
+
+    public double BriquetteSp { get; set; }
+
+    public double ReserveSp { get; set; }
+
+    public double CokeSp { get; set; }
+}
---
+++ LoadOfMaterials/Models/StatsContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoadOfMaterials.Models;
+
+public partial class StatsContext : DbContext
+{
+    public StatsContext()
+    {
+    }
+
+    public StatsContext(DbContextOptions<StatsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<LoadingMaterial> LoadingMaterials { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=stats;Username=postgres;Password=Lemon4ik62212006");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LoadingMaterial>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("LoadingMaterials_pkey");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.TimeNum).HasColumnType("timestamp without time zone");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
