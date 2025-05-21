153f69e0251da5f7df617aedff16c2682467c534
Александр Ольшанский <alex.olshanskii2006@gmail.com>
2025-05-21 15:25:06 +0500

    Главная форма

---
+++ LoadOfMaterials/FormMain.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace LoadOfMaterials
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelButtons = new Panel();
+            button1 = new Button();
+            panel1 = new Panel();
+            button2 = new Button();
+            dataGridView = new DataGridView();
+            panelButtons.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // panelButtons
+            // 
+            panelButtons.Controls.Add(button2);
+            panelButtons.Controls.Add(button1);
+            panelButtons.Dock = DockStyle.Top;
+            panelButtons.Location = new Point(0, 0);
+            panelButtons.Name = "panelButtons";
+            panelButtons.Padding = new Padding(10);
+            panelButtons.RightToLeft = RightToLeft.Yes;
+            panelButtons.Size = new Size(1209, 86);
+            panelButtons.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(13, 13);
+            button1.Name = "button1";
+            button1.Size = new Size(189, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Вывести таблицу";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(dataGridView);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 86);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(10);
+            panel1.Size = new Size(1209, 735);
+            panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.Location = new Point(208, 13);
+            button2.Name = "button2";
+            button2.Size = new Size(189, 60);
+            button2.TabIndex = 1;
+            button2.Text = "Вывести график";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Dock = DockStyle.Fill;
+            dataGridView.Location = new Point(10, 10);
+            dataGridView.Name = "dataGridView";
+            dataGridView.Size = new Size(1189, 715);
+            dataGridView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1209, 821);
+            Controls.Add(panel1);
+            Controls.Add(panelButtons);
+            Name = "Form1";
+            Text = "FormMain";
+            panelButtons.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelButtons;
+        private Button button1;
+        private Panel panel1;
+        private Button button2;
+        private DataGridView dataGridView;
+    }
+}
